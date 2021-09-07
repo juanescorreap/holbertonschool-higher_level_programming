8ca063e204b0c62a95ef98539a68a4673c704350
juanescorreap <juanestebancope@outlook.com>
2021-09-07 13:21:04 -0500

    conditional for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,15 @@
 import random
 from typing import AbstractSet
 number = random.randint(-10000, 10000)
-abs = abs(number)
-if number % 10 > 5:
+if number < 0:
+    x =  -10
+else:
+    x = 10
+if number % x > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(
-        number, abs % 10))
-elif number % 10 == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, abs % 10))
-elif number % 10 < 6 and number % 10 != 0:
+        number, number % x))
+elif number % x == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, number % x))
+elif number % x < 6 and number % x != 0:
     print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(
-        number, abs % 10))
+        number, number % x))
