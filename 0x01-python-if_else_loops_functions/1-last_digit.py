cfc6dd0bbe1f104d724ef63b765cd84fd441a307
juanescorreap <juanestebancope@outlook.com>
2021-09-07 12:13:49 -0500

    Conditional added for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    number = number *-1
 if number % 10 > 5:
     print("Last digit of {:d} is {:d} and is greater than 5".format(
         number, number % 10))
