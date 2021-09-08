b33a1cece73ff0123b1294c9cb13d27f5baaafa3
juanescorreap <juanestebancope@outlook.com>
2021-09-08 16:33:10 -0500

    convert tabs to spaces

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import hdden_4
-	for element in dir(hidden_4):
-		if element[0:2] != "__":
-			print("{:s}".format(element))
+    import hdden_4
+    for element in dir(hidden_4):
+        if element[0:2] != "__":
+            print("{:s}".format(element))
