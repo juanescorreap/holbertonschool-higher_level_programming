197f0c02ef29becce4622138175d76aebbd143db
juanescorreap <juanestebancope@outlook.com>
2021-09-13 16:54:37 -0500

    comprehension instead of slicing

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    while (i < len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            my_string = my_string[:i] + my_string[i+1:]
-        i = i + 1
-    return(my_string)
+    new_string = [i for i in my_string if i != 'c' and i != 'C']
+    return ("".join(new_string))
