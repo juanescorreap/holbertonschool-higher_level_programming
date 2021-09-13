7125808516a9b50a43db22be04427cb073385ef6
juanescorreap <juanestebancope@outlook.com>
2021-09-13 16:04:36 -0500

    una de dos

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for val in row:
             print("{:d}".format(val), end="")
             if val != row[-1]:
-                print(" ",end="")
-        print()
+                print(" ", end="")
+        print("")
