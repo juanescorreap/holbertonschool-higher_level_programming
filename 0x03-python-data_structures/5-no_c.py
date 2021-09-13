57df39a2dba9fb2464faa9c4068fd52bb8494ff6
juanescorreap <juanestebancope@outlook.com>
2021-09-13 15:44:36 -0500

    pep8 style

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     while (i < len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
             my_string = my_string[:i] + my_string[i+1:]
-    i = i + 1
+        i = i + 1
     return(my_string)
