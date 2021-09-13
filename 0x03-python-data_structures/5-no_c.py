790e2119039f49071367b5e8319de00d8aa9be04
juanescorreap <juanestebancope@outlook.com>
2021-09-13 15:53:47 -0500

    new string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     new_string = my_string.copy()
     i = 0
-    while (i < len(new_string)):
-        if new_string[i] == 'c' or new_string[i] == 'C':
-            new_string = new_string[:i] + new_string[i+1:]
+    while (i < len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            new_string = my_string[:i] + my_string[i+1:]
         i = i + 1
     return(new_string)
