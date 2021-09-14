13bb35b71b5d03f56e7c5d92f7c79a830fc5bf57
juanescorreap <juanestebancope@outlook.com>
2021-09-13 19:11:11 -0500

    change in when to initialize biggest

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    biggest = my_list[0]
     if my_list:
+        biggest = my_list[0]
         for i in my_list:
             if biggest <= i:
                     biggest = i
