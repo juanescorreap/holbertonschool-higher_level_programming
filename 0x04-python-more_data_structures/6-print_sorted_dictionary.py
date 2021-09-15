41df2b754d40fd6a261c03664fca24be1d65fb3c
juanescorreap <juanestebancope@outlook.com>
2021-09-14 21:32:20 -0500

    unknown modification

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,3 @@ def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
         sorted_dictionary[i] = a_dictionary[i]
         print("{}: {}".format(i, sorted_dictionary[i]))
-
