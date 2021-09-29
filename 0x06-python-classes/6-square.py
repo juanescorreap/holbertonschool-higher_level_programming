d90bd47351d4223bca023896932ac925d5ec484b
John Doe <juanestebancope@outlook.com>
2021-09-29 18:16:28 -0500

    delete underscore

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@
 class Square:
     """Class that defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """Public instance method that returns the current square area"""
