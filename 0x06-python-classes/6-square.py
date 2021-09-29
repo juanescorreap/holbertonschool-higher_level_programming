74a22eb41d0365a017b207491eb14cb21e66cfe7
John Doe <juanestebancope@outlook.com>
2021-09-29 17:12:31 -0500

    change _ for space

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
         for j in range(0, self.__position[1]):
             print()
         for i in range(0, self.__size):
-            print('_' * self.__position[0], end="")
+            print(" " * self.__position[0], end="")
             for i in range(0, self.__size):
                 print('#', end="")
             print()
