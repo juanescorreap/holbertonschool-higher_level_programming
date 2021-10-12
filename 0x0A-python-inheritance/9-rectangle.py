85092acbc44e76019685caefd3cf2ced7f505f45
John Doe <juanestebancope@outlook.com>
2021-10-11 22:38:28 -0500

    nose

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -8,8 +8,8 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Class that defines Rectangle"""
     def __init__(self, width, height):
-        super().integer_validator(self, "width", width)
-        super().integer_validator(self, "height", height)
+        BaseGeometry.integer_validator(self, "width", width)
+        BaseGeometry.integer_validator(self, "height", height)
         self.__width = width
         self.__height = height
 
