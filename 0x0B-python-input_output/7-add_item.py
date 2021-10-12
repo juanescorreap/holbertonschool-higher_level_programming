7420d61fdc72910e88d907708d5e325c703550d0
John Doe <juanestebancope@outlook.com>
2021-10-12 18:33:46 -0500

    for indentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     validator = os.path.exists("add_item.json")
     if validator is True:
         my_list = load_from_json_file("add_item.json")
-        for i in sys.argv[1:]:
-            my_list.append(i)
+    for i in sys.argv[1:]:
+        my_list.append(i)
 
     save_to_json_file(my_list, "add_item.json")
