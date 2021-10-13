06331df74216bfd1608c62d878eb9f292f33835e
John Doe <juanestebancope@outlook.com>
2021-10-12 19:21:40 -0500

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -10,4 +10,4 @@ def class_to_json(obj):
     """Function that returns the dictionary description
     with simple data structure (list, dictionary, string, integer and boolean)
     for JSON serialization of an object"""
-    return(dict(obj))
+    return(obj.__dict__)
