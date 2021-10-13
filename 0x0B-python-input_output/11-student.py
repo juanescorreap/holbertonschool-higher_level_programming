e62ad3159c644bb1def42d42c988671a02e6eb08
John Doe <juanestebancope@outlook.com>
2021-10-12 20:44:51 -0500

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Class that defines Student"""
+
+
+class Student:
+    """Class that defines Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return(self.__dict__)
+        else:
+            return({i: self.__dict__[i] for i in self.__dict__ if i in attrs})
+
+    def reload_from_json(self, json):
+        for i in json:
+            if i in self.__dict__:
+                self.__dict__[i] = json[i]
