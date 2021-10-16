0ca5f38d3beea063f9a13d7abd94469088e1692d
John Doe <juanestebancope@outlook.com>
2021-10-16 15:53:59 -0500

    up to task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,10 +27,23 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         filename = str(cls.__name__+".json")
+        my_list = []
         if list_objs is None:
-            my_list= []
             with open(filename, mode="w", encoding="utf-8") as my_file:
                 my_file.write(my_list)
         else:
             with open(filename, mode="w", encoding="utf-8") as my_file:
-                my_file.write(cls.to_json_string(list_objs))
+                for i in range(len(list_objs)):
+                    my_list.append(cls.to_dictionary(list_objs[i]))
+                my_file.write(cls.to_json_string(my_list))
+    @staticmethod
+    def from_json_string(json_string):
+        my_list = []
+        if json_string is None or bool(json_string) is False:
+            return(my_list)
+        else:
+            return(json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        
