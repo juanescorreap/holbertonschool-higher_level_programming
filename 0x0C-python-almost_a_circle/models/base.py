3e800ab179dcd97fdcfd3a7781fbb502124b1c78
John Doe <juanestebancope@outlook.com>
2021-10-18 15:47:22 -0500

    little change

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,11 +20,9 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or bool(list_dictionaries) is False:
+        if list_dictionaries is None:
             list_dictionaries = []
-            return(list_dictionaries)
-        else:
-            return(json.dumps(list_dictionaries))
+        return(json.dumps(list_dictionaries))
 
     @classmethod
     def save_to_file(cls, list_objs):
