387aedce7a27f47da3c0a7df02ef1248d27c381f
John Doe <juanestebancope@outlook.com>
2021-10-18 19:16:52 -0500

    some changes

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -854,32 +854,4 @@ class Test_base(unittest.TestCase):
         r1 = Rectangle(4, 6)
         self.assertEqual(r1.display(), None)
 
-    def test_display(self):
-        """Test display with valid arguments"""
-        # creation of file that stores the
-        # representation of display() in the future
-        f = io.StringIO()
-        s = ('#' * 4 + '\n') * 3
-        r1 = Rectangle(4, 3)
-        with redirect_stdout(f):
-            # the result of display stores the content in
-            # f accessing to his content with getvalue
-            r1.display()
-        # compare s and f
-        self.assertEqual(f.getvalue(), s)
-
-    def test_display_valid(self):
-        """Test display with valid arguments"""
-        file = io.StringIO()
-        expected = ('#' * 32 + '\n') * 32
-        r1 = Rectangle(32, 32)
-        with redirect_stdout(file):
-            r1.display()
-        self.assertEqual(file.getvalue(), expected)
-
-        file = io.StringIO()
-        expected = ('#' * 2 + '\n') * 52
-        r2 = Rectangle(2, 52)
-        with redirect_stdout(file):
-            r2.display()
-        self.assertEqual(file.getvalue(), expected)
+    
