774660c6102363d30c578b331034247fda470255
John Doe <juanestebancope@outlook.com>
2021-11-23 21:30:56 -0500

    delete DISTINCT and add ORDER BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,7 @@
 --  script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT score
-COUNT(DISTINCT score)
+COUNT(score)
 AS number
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC;
