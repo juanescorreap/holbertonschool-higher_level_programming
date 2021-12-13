9c2b0fc7ce8f15cc017a50bcceaa844c92e85a99
John Doe <juanestebancope@outlook.com>
2021-12-13 15:41:26 -0500

    semistandard style

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 // script that prints a message depending of the number of arguments passed
-if (process.argv.length == 2)
-{
-	console.log('No argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
 }
-if (process.argv.length == 3)
-{
-	console.log('Argument found');
+if (process.argv.length === 3) {
+  console.log('Argument found');
 }
-if(process.argv.length > 3)
-{
-	console.log('Arguments found');
+if (process.argv.length > 3) {
+  console.log('Arguments found');
 }
