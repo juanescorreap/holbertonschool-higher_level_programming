bc74156de3860abe4f5db16a7df7f12c86fa32f0
John Doe <juanestebancope@outlook.com>
2021-12-13 21:09:46 -0500

    add if and else statements

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments
-const myArray = [];
-let i = 2;
-while (process.argv[i]) {
-  myArray[i - 2] = parseInt(process.argv[i]);
-  i = i + 1;
+if (process.argv.length === 2) {
+  console.log('0');
 }
-const max = Math.max.apply(null, myArray);
-myArray.splice(myArray.indexOf(max), 1);
-console.log(Math.max.apply(null, myArray));
+if (process.argv.length === 3) {
+  console.log('1');
+} else {
+  const myArray = [];
+  let i = 2;
+  while (process.argv[i]) {
+    myArray[i - 2] = parseInt(process.argv[i]);
+    i = i + 1;
+  }
+  const max = Math.max.apply(null, myArray);
+  myArray.splice(myArray.indexOf(max), 1);
+  console.log(Math.max.apply(null, myArray));
+}
