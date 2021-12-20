cea35e3a1634d2bd03a501289dba7600e2edae04
John Doe <juanestebancope@outlook.com>
2021-12-20 15:19:35 -0500

    make file executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-	database = MySQLdb.connect(host='localhost', port=3306,
-							user=argv[1], passwd=argv[2], db=argv[3])
-	cursor = database.cursor()
-	cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
-	for states in cursor.fetchall():
-		print(states)
-		cursor.close()
-		database.close()
+    database = MySQLdb.connect(host='localhost', port=3306,
+                               user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = database.cursor()
+    cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
+    for states in cursor.fetchall():
+        print(states)
+        cursor.close()
+        database.close()
