8f5ad7b2cb56ee6ca6a94131f32aa38fec612ac4
John Doe <juanestebancope@outlook.com>
2021-12-20 15:18:13 -0500

    add validation for importation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,12 +5,12 @@ from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
-
-database = MySQLdb.connect(host='localhost', port=3306,
-                           user=argv[1], passwd=argv[2], db=argv[3])
-cursor = database.cursor()
-cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
-for states in cursor.fetchall():
-    print(states)
-    cursor.close()
-    database.close()
+if __name__ == "__main__":
+	database = MySQLdb.connect(host='localhost', port=3306,
+							user=argv[1], passwd=argv[2], db=argv[3])
+	cursor = database.cursor()
+	cursor.execute('SELECT id, name FROM states ORDER BY states.id ASC')
+	for states in cursor.fetchall():
+		print(states)
+		cursor.close()
+		database.close()
