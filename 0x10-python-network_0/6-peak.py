e61b1993b36ceeb4c1c70fec8742751ba2027459
John Doe <juanestebancope@outlook.com>
2022-01-09 18:45:17 -0500

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Function that finds a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    """Function that finds a peak in a list of unsorted integers"""
+    n = len(list_of_integers)
+    left = 0
+    right = n - 1
+    while left <= right:
+        mid = left + right
+        if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and (mid == n-1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return mid
+        if mid == 0 or list_of_integers[mid - 1] > list_of_integers[mid]:
+            right = mid -1
+        else:
+            left = mid + 1
+            
