1906aea5ab6297007c2532b762373e13adc97a31
John Doe <juanestebancope@outlook.com>
2022-01-09 18:47:56 -0500

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,7 +9,7 @@ def find_peak(list_of_integers):
     left = 0
     right = n - 1
     while left <= right:
-        mid = left + right
+        mid = (left + right) // 2
         if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and (mid == n-1 or list_of_integers[mid] >= list_of_integers[mid + 1]):
             return mid
         if mid == 0 or list_of_integers[mid - 1] > list_of_integers[mid]:
