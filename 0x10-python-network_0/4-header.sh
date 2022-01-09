7a9c7e0948200186fd985a83fb0cf3dd42aebc9c
John Doe <juanestebancope@outlook.com>
2022-01-09 17:05:06 -0500

    add newline at the end of file

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -H "X-School-User-Id: 98"-siX GET "$1"
+curl -H 'X-School-User-Id: 98'-siX GET "$1"
