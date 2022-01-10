e1d7f2c2058b37e86a8cf01fe9ef9b63fc4503f1
John Doe <juanestebancope@outlook.com>
2022-01-09 19:53:29 -0500

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,8 @@
 
 def find_peak(list_of_integers):
     """Function that finds a peak in a list of unsorted integers"""
-def find_peak(list_of_integers):
-    if list_of_integers:
-        return max(list_of_integers)
-    else:
-        return "None"
+
+    if list_of_integers is None or len(list_of_integers) < 1:
+        return None
+    maximum = max(list_of_integers)
+    return maximum
