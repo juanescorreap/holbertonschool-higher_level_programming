0bbd31f33cf6e8a13dbed9119e080b4b6a4a1a4c
John Doe <juanestebancope@outlook.com>
2022-01-10 15:47:10 -0500

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,8 +10,7 @@ url = sys.argv[1]
 values = {'email': sys.argv[2]}
 
 data = urllib.parse.urlencode(values)
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
+data = data.encode("utf-8")
+with urllib.request.urlopen(url, data) as response:
     the_page = response.read().decode("utf-8")
 print(the_page)
