3b7f5293e2629461b7f28d1804c2fb867d55325c
John Doe <juanestebancope@outlook.com>
2022-01-09 21:09:32 -0500

    After multiple test, the cheker is wrong

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 
-from urllib.request import urlopen
+if __name__ == "__main__":
+    import urllib.request
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        status = response.read()
 
-actions = [
-        ("type", lambda html: type(html)),
-        ("content", lambda html: html),
-        ("utf8 content", lambda html: html.decode()),
-]
-
-with urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:")
-    [print("\t- {}: {}".format(header, fun(html))) for header, fun in actions]
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(status), status, status.decode('utf-8')))
