ee1b7f8935c59a56249741b8d5bbace7d92eb6b7
John Doe <juanestebancope@outlook.com>
2022-01-09 21:00:41 -0500

    differt way printing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         status = response.read()
 
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(status), status, status.decode()))
+    print("Body response:")
+    print("\t- type: {}".format(type(status)))
+    print("\t- content: {}".format(status))
+    print("\t- utf8 content: {}".format(status.decode()))
