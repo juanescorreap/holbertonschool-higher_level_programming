f6f5e6ea2cc35a4ff375c5a15c77264f76714eaa
John Doe <juanestebancope@outlook.com>
2022-01-10 16:23:31 -0500

    add Request method before try statement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,15 @@
 """Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 
+from sys import argv
+
+
 if __name__ == "__main__":
     import urllib.request
     import sys
+    req = urllib.request.Request(argv[1])
 try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
+    with urllib.request.urlopen(req) as response:
         the_page = response.read().decode("utf-8")
         print(the_page)
 except urllib.error.HTTPError as error:
