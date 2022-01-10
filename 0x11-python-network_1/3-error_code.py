2980f68e2ffb08966b1f0a6179094754a1f4de03
John Doe <juanestebancope@outlook.com>
2022-01-10 16:29:53 -0500

    move module imports to different line

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
+import urllib.request
+import sys
 
 if __name__ == "__main__":
-    import urllib.request
-    import sys
     req = urllib.request.Request(sys.argv[1])
 try:
     with urllib.request.urlopen(req) as response:
