dca787212f630d01214e4ea38d9e9c20b024fed8
John Doe <juanestebancope@outlook.com>
2022-01-10 16:27:52 -0500

    sys.argv[1]

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,9 +2,6 @@
 """Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 
-from sys import argv
-
-
 if __name__ == "__main__":
     import urllib.request
     import sys
