b00cc4b13a98f728f114647a5f2a88e764de3e7f
John Doe <juanestebancope@outlook.com>
2022-01-09 20:45:43 -0500

    pep8 style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         status = response.read()
 
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
-        type(status), status, status.decode('utf-8')))
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(status), status, status.decode('utf-8')))
