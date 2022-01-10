c5c663c31998311995f59e7ce50dc4ac10cbdfa1
John Doe <juanestebancope@outlook.com>
2022-01-10 18:15:31 -0500

    validate entry

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,11 +2,19 @@
 """Python script that takes in a letter and sends a POST request
 to http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
+from sys import argv
+
+
 if __name__ == "__main__":
     import requests
     import sys
+    if len(argv) < 2:
+        value = ""
+    else:
+        value = sys.argv[1]
+
     r = requests.post('http://0.0.0.0:5000/search_user',
-                      data={'q': sys.argv[1]})
+                      data={'q': value})
     try:
         answer = r.jason()
         if len(answer) == 2:
