8ee9df9e094b6c298c4ab0d4a5f1ee72ce778eee
John Doe <juanestebancope@outlook.com>
2022-01-09 20:32:20 -0500

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Python script that fetches https://intranet.hbtn.io/status"""
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    status = response.read()
+
+print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(type(status), status, status.decode('utf-8')))
