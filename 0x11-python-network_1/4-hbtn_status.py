d3d5224ac74fd4e4d4166833ccc782915ca91211
John Doe <juanestebancope@outlook.com>
2022-01-10 16:49:25 -0500

    add r.text

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,6 +4,6 @@
 if __name__ == "__main__":
     import requests
     r = requests.get('https://intranet.hbtn.io/status')
-
+    r = r.text
     print("Body response:\n\t- type: {}\n\t- content: {}"
           .format(type(r), r))
