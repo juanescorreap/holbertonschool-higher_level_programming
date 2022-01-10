91206f92e34097fd91e62991ae6c57d59d10ebab
John Doe <juanestebancope@outlook.com>
2022-01-09 20:56:11 -0500

    status.decode

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         status = response.read()
 
-    print(f"Body response: \n\t - type: {type(status)}\n\t - content:\
- {status}\n\t - utf8 content: {status.decode()}")
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    .format(type(status), status, status.decode()))
